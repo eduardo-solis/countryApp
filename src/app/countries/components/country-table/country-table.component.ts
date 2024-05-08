cb7f35ba7daadf810cb2ad6427ebf43ef79d6518
Eduardo Solis <solishernandezeduardoruben@gmail.com>
2024-05-08 17:03:10 -0600

    Creando el componente countryTable

---
+++ src/app/countries/components/country-table/country-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'countries-table',
+  templateUrl: './country-table.component.html',
+  styles: ``
+})
+export class CountryTableComponent {
+
+}
