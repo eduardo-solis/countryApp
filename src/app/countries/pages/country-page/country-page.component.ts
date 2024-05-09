b1e6514b109f43e95e2732b0c33a2eee33663b9c
Eduardo Solis <solishernandezeduardoruben@gmail.com>
2024-05-08 19:59:28 -0600

    Ajustando el metodo searchCountryByAlphaCode del servicio

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'countries-country-page',
@@ -9,23 +11,23 @@ import { CountriesService } from '../../services/countries.service';
 })
 export class CountryPageComponent implements OnInit {
 
+  public country?: Country;
+
   constructor( 
       private activatedRoute: ActivatedRoute,
+      private router: Router,
       private countriesService: CountriesService
   ){}
 
   ngOnInit(): void {
     
     this.activatedRoute.params
-      .subscribe( ({id}) => {
-
-        this.countriesService.searchCountryByAlphaCode(id)
-          .subscribe( country => {
-            console.log(country);
-            
-          })
-        
-
+      .pipe(
+        switchMap( ({id}) => this.countriesService.searchCountryByAlphaCode(id) )
+      )
+      .subscribe( country => {
+        if( !country ) return this.router.navigateByUrl('');
+        return this.country = country;
       } )
 
   }
