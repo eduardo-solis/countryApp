e4144dbcc19a7076c728e9618bfa0bfa002b9b94
Eduardo Solis <solishernandezeduardoruben@gmail.com>
2024-05-11 15:11:50 -0600

    Creando y usando un debounce en las peticiones

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,23 +1,45 @@
-import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styles: ``
 })
-export class SearchBoxComponent {
-
+export class SearchBoxComponent implements OnInit {
+  
+  private debouncer: Subject<string> = new Subject<string>();
+  
   @Input()
   public placeholder: string = '';
-
+  
   @Output()
   public onValue: EventEmitter<string> = new EventEmitter();
 
+  @Output()
+  public onDebounce: EventEmitter<string> = new EventEmitter();
+  
   @ViewChild('txtInput')
   public txtInput!: ElementRef<HTMLInputElement>;
+  
 
+  ngOnInit(): void {
+    this.debouncer
+      .pipe(
+        debounceTime(500)
+      )
+      .subscribe( value => {
+        this.onDebounce.emit(value);
+      })
+  }
+
+  
   emitValue( value: string ): void {
     this.onValue.emit(value);
+  }
+
+  public onKeyPress( searchTerm: string ):void {
+    this.debouncer.next(searchTerm);
   }
 
 }
