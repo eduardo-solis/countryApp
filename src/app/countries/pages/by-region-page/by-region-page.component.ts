c42e72fd394d8a8447121e0d9dfc1fe5ddeaf606
Eduardo Solis <solishernandezeduardoruben@gmail.com>
2024-05-12 13:00:29 -0600

    Optimización en la busqueda de la region

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country.interface';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa'|'Americas'|'Asia'|'Europe'|'Oceania';
+
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -10,13 +12,16 @@ import { CountriesService } from '../../services/countries.service';
 export class ByRegionPageComponent {
 
   public countries: Country[] = [];
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
   public isLoading: boolean = false;
 
   constructor( private countriesService: CountriesService ){}
 
-  searchByRegion( region: string ): void {
+  searchByRegion( region: Region ): void {
 
     this.isLoading = true;
+    this.selectedRegion = region;
 
     this.countriesService.searchRegion(region)
         .subscribe( countries => { 
